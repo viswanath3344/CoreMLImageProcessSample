89cd09a65fa1176fc212650c22af19740b634c8b
viswanath reddy <viswanath3344@gmail.com>
2018-05-15 11:52:40 +0530

    Added project with Sample done

--- CoreMLSample/ViewController.swift
+++ CoreMLSample/ViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+   var objGoogleNetPlacesModel = GoogLeNetPlaces()
+    
+    @IBOutlet weak var imageTypeLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +21,41 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
 
+    // Called this method when tap on Image
+    @IBAction func tappedOnImage(sender:UITapGestureRecognizer)
+    {
+        let imageView  = sender.view as? UIImageView
+        if let  image  = imageView?.image
+        {
+           if let placeTypeString  =  imageTypeDetection(image:image)
+           {
+                imageTypeLabel.text = placeTypeString
+            
+            }
+            
+            
+        }
+        
+    }
+    
+    func imageTypeDetection(image:UIImage) -> String? {
+        
+        // Getting  pixelBuffer by Image Data.
+        if let pixelBuffer = ImageProcessor.pixelBuffer(forImage: image.cgImage!)
+        {
 
+            // Getting place type by supplying pixelBuffer.
+            guard let type =  try? objGoogleNetPlacesModel.prediction(sceneImage: pixelBuffer) else {fatalError("Un Expected")}
+            
+            return type.sceneLabel
+            
+        }
+        
+        return nil
+    }
+
+    
 }
 
